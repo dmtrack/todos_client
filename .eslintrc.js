eecb88035ad8f4110ea5d1f0eb93c316121f8b35
dmtrack <rightrack888@gmail.com>
2023-12-09 09:21:59 +0500

    feat: jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: ['plugin:react/recommended', 'airbnb'],
     parser: '@typescript-eslint/parser',
@@ -37,6 +38,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
